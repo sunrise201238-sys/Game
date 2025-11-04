fdc8f4854f8e1fe6601a6969e0cf61d193fe9d36
sunrise201238-sys <sunrise201238@gmail.com>
2025-11-04 14:45:56 +0800

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -307,7 +307,7 @@ const RIFT_BASE_TR: XY = { x: RIFT_WIDTH - 112, y: 96 };
 // Defender cluster offset from the base, inspired by the reference layout (bottom-left version)
 const RIFT_DEFENDER_OFFSETS: XY[] = [
   { x: -25, y: -225 },
-  { x: 25, y: -225 },
+  { x: 25, y: -250 },
   { x: 165, y:  -61 },
   { x: 215, y:  -61 },
   { x: 325, y:  34 },
