3c377cc610ece538526155b569a19399289990d5
sunrise201238-sys <sunrise201238@gmail.com>
2025-11-05 16:12:55 +0800

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -295,7 +295,7 @@ const riftEdgeWalls: Rect[] = [
   // bottom
   horizWall(RIFT_X_LEFT + 140 + 108, RIFT_Y_BOTTOM - 18, (RIFT_X_RIGHT - RIFT_X_LEFT - 280) - 108, 36),
   // left
-  vertWall(RIFT_X_LEFT - 18, RIFT_Y_TOP + 273, 36, (RIFT_Y_BOTTOM - RIFT_Y_TOP - 320) - 108),
+  vertWall(RIFT_X_LEFT - 18, RIFT_Y_TOP + 280, 36, (RIFT_Y_BOTTOM - RIFT_Y_TOP - 320) - 72),
   // right
   vertWall(RIFT_X_RIGHT - 18, RIFT_Y_TOP + 160, 36, (RIFT_Y_BOTTOM - RIFT_Y_TOP - 320) -108),
 ];
