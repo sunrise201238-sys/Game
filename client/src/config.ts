79f06efe09c11f11a5118d4b416f694c704a5cf4
sunrise201238-sys <sunrise201238@gmail.com>
2026-04-25 19:07:45 +0900

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -106,7 +106,7 @@ export const UNIT_DEFINITIONS: UnitDefinition[] = [
     name: 'VIP',
     color: '#facc15',
     radius: 14,
-    maxHp: 85,
+    maxHp: 150,
     collideDamage: 0,
     knockback: 0,
     recoil: 0,
